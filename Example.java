6bdf2eb9e7e00e51357fb8ca5032be00c46ed0c8
Tunde Idahor <bidahor13@wou.edu>
2016-09-15 23:51:01 -0700

    Treet

---
+++ Example.java
@@ -0,0 +1,38 @@
+
+import com.teamTunde.Treet;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Example {
+   public static void main(String []args){
+     
+       /* 1st Sample treet */
+      Treet treet = new Treet(   "Max Payne", 
+                                 "You want? to be famous just@ tweet$ about me_ heheh!!" , 
+                                 new Date(1473919826000L) 
+                             );
+     
+       /*2nd Sample treet */
+      Treet secondTreet = new Treet(  "Mad Max", 
+                                      "Zumber..  about me_ heheh!!" , 
+                                       new Date(1473919826000L) 
+                                   );
+     
+     System.out.printf("This is a new treet: %s %n", treet);
+     System.out.println("The words are: ");
+     
+     /* searches and print out all the words in the sentence. */
+     for(String word: treet.getWords()){
+       System.out.println(word);
+     }
+     
+     /* stores each treet and sorts. */
+     Treet[] treets = {treet, secondTreet};
+     Arrays.sort(treets);
+     for (Treet exTreet : treets){
+       System.out.println( exTreet );
+     }
+   }
+  
+}
---
+++ com/teamTunde/Treet.java
@@ -0,0 +1,61 @@
+package com.teamTunde;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Treet implements Comparable{
+  
+  private String mAuthor;
+  private String mDescription;
+  private Date mCreateDate;
+  
+  public Treet(String author,String description, Date createDate ){
+     mAuthor = author;
+     mDescription = description;
+     mCreateDate = createDate;
+  }
+  
+  
+  
+  public String getAuthor(){
+    return mAuthor;
+  }
+  
+
+  @Override
+  public String toString(){
+    return String.format( "Tweet:  \"%s\" by %s on %s",  mDescription, mAuthor, mCreateDate);
+  }
+  
+  
+  @Override
+  public int compareTo(Object obj){
+    Treet other = (Treet) obj;
+    if(equals(other)){
+      return 0;
+    }
+     int dateCmp =  mCreateDate.compareTo(other.mCreateDate);
+     if(dateCmp == 0){
+     return mDescription.compareTo(other.mDescription);
+    }
+     return  dateCmp;
+  }
+  
+  
+  public String getDescription(){
+    return  mDescription;
+  }
+  
+  
+  public String[] getWords(){
+   return mDescription.toLowerCase().split(" ");
+  }
+  
+  
+  
+   public Date getCreateDate(){
+    return  mCreateDate;
+  }
+  
+
+}
